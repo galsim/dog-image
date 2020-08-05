4f631c9f8f433411e28f0a796a6aa61f44e44fc3
galsim <Vjrbc05121996>
2020-08-05 22:01:28 +0300

    project init

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+      <h1>Hello, world!</h1>
+      <p>{{ getCount }}</p>
+      <button @click="increment">Увеличить</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  mounted () {
+    console.log(this.$store)
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getCount'
+    ])
+  }
+}
+</script>
